f9359e430f74a52798d44f9418b815e3b05b5cbb
mli42 <mli@student.42.fr>
2022-06-22 16:34:08 +0200

    Plot loss (mse)

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import utils
+from typing import List
 
 class MyLinearRegression():
 
@@ -11,57 +12,38 @@ class MyLinearRegression():
         print(f"MyLR: got theta {utils.str_array(self.theta)}")
 
     @staticmethod
-    def mse_(y: np.ndarray, y_hat: np.ndarray) -> float:
-        if y.shape != y_hat.shape:
-            return None
-        mse_elem = (y_hat - y) ** 2 / (y.shape[0])
-        return np.sum(mse_elem)
-
-    @staticmethod
-    def cost_elem_(y: np.ndarray, y_hat: np.ndarray) -> np.ndarray:
-        """
-        Description:
-            Calculates all the elements (1/2*M)*(y_pred - y)^2 of the cost function.
-        Args:
-            y: has to be an numpy.ndarray, a vector.
-            y_hat: has to be an numpy.ndarray, a vector.
-        Returns:
-            J_elem: numpy.ndarray, a vector of dimension (number of the training examples,1).
-            None if there is a dimension matching problem between X, Y or theta.
-        Raises:
-            This function should not raise any Exception.
-        """
-        if y.shape != y_hat.shape:
-            return None
-        res = (y_hat - y) ** 2 / (2 * y.shape[0])
-        return res
-
-    @staticmethod
     def cost_(y: np.ndarray, y_hat: np.ndarray) -> float:
-        """Computes the half mean squared error of two non-empty numpy.ndarray,
-            without any for loop. The two arrays must have the same dimensions.
+        """Computes the mean squared error of two non-empty numpy.ndarray.
+            The two arrays must have the same dimensions.
         Args:
             y: has to be an numpy.ndarray, a vector.
             y_hat: has to be an numpy.ndarray, a vector.
         Returns:
-            The half mean squared error of the two vectors as a float.
-            None if y or y_hat are empty numpy.ndarray.
-            None if y and y_hat does not share the same dimensions.
-        Raises:
-            This function should not raise any Exceptions.
+            The mean squared error (MSE) of the two vectors as a float.
         """
         if y.shape != y_hat.shape:
             return None
-        j_elem = MyLinearRegression.cost_elem_(y, y_hat)
+        j_elem = (y_hat - y) ** 2 / y.shape[0]
         return np.sum(j_elem)
 
-    def plot(self, x: np.ndarray, y: np.ndarray, y_hat: np.ndarray) -> None:
+    def plot(self, x: np.ndarray, y: np.ndarray, y_hat: np.ndarray, losses: List[float]) -> None:
+        plt.figure()
+        # Data repartition
         plt.plot(x, y, "o")
+        # Prediction model
         plt.plot(x, y_hat)
-
+        plt.title('Data repartition and predition model')
         plt.legend(['Dataset','Hypothesis'])
         plt.xlabel("Mileage (km)")
         plt.ylabel("Price of car")
+
+        plt.figure()
+        plt.title('Train loss through epochs')
+        plt.plot(losses)
+        plt.legend(['Loss'])
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+
         plt.show()
 
     def minmax(self, x: np.ndarray) -> np.ndarray:
@@ -104,12 +86,19 @@ class MyLinearRegression():
         if x.shape != y.shape or self.theta.shape != (2, 1):
             return None
         norm_x = self.minmax(x)
+        losses = []
         for _ in range(self.max_iter):
             gradient = self.gradient(norm_x, y)
             self.theta -= gradient
+
+            running_loss = MyLinearRegression.cost_(y, utils.predict(norm_x, self.theta))
+            losses.append(running_loss)
+
         utils.save_theta(self.theta)
         y_hat = utils.predict(norm_x, self.theta)
-        self.plot(x, y, y_hat)
+        print(f"Last loss: {losses[-1]}")
+
+        self.plot(x, y, y_hat, losses)
 
 def main():
     mylr = MyLinearRegression(0.1)
